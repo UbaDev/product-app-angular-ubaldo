effad8e72cbe766794bd56fa004755cf17301cde
Ubaldo <162737358+UbaldoMedina@users.noreply.github.com>
2025-06-16 16:46:26 -0600

    fix: delete animations

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    importProvidersFrom(BrowserAnimationsModule)
+    importProvidersFrom()
   ]
 }).catch(err => console.error(err));
