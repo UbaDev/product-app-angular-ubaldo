7e49cf317b89fa1752089fb5c72cfe8ebeabc39e
Ubaldo <162737358+UbaldoMedina@users.noreply.github.com>
2025-06-16 16:38:26 -0600

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(BrowserAnimationsModule)
   ]
 }).catch(err => console.error(err));
